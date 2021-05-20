031b86180c63766ded232fd59441fa05d77543a6
Devin Singh <drsingh2518@icloud.com>
2021-05-20 18:52:28 -0500

    fix compile
    closes #1

--- aakil/src/arm9/coprocessor.rs
+++ aakil/src/arm9/coprocessor.rs
@@ -21,8 +21,7 @@ impl Cp15 {
         let mut tcm_size_reg = Register::new("TCM Size Register");
         tcm_size_reg.value = 0x00140180;
         let control_reg = ControlRegister::from(0x00000078);
-        let mut protection_unit = Register::new("Protection Unit");
-        protection_unit.value = 0x00140180;
+        let protection_unit = ProtectionUnit {};
         Self {
             main_id_reg,
             cache_type_reg,
@@ -89,61 +88,7 @@ pub(super) struct ControlRegister {
     __: B2,
 }
 
-#[bitfield]
-#[repr(u32)]
-#[derive(Debug)]
-pub(super) struct ProtectionUnit {
-    mmu_pu_enable: B1,
-    #[skip(setters)]
-    alignment_fault_check: B1,
-    data_unified_cache: B1,
-    #[skip(setters)]
-    write_buffer: B1,
-    #[skip(setters)]
-    exception_handling: B1,
-    #[skip(setters)]
-    _26bit_address_faults: B1,
-    #[skip(setters)]
-    abort_model: B1,
-    #[bits = 1]
-    endianness: Endianness,
-    #[skip(setters)]
-    sys_protection: B1,
-    #[skip(setters)]
-    rom_protection: B1,
-    #[skip(setters)]
-    impl_defined: B1,
-    #[skip(setters)]
-    branch_prediction: B1,
-    instruction_cache: B1,
-    exception_vectors: B1,
-    cache_replacement: B1,
-    pre_armv5: B1,
-    dtcm_enabled: B1,
-    dtcm_load_mode: B1,
-    itcm_enabled: B1,
-    itcm_load_mode: B1,
-    #[skip]
-    __: B2,
-    #[skip(setters)]
-    unaligned_access: B1,
-    #[skip(setters)]
-    extended_page_table: B1,
-    #[skip]
-    __: B1,
-    #[skip(setters)]
-    cpsr_e_on_exception: B1,
-    #[skip]
-    __: B1,
-    #[skip(setters)]
-    fiq_behavior: B1,
-    #[skip(setters)]
-    tex_remap: B1,
-    #[skip(setters)]
-    force_ap: B1,
-    #[skip]
-    __: B2,
-}
+pub(super) struct ProtectionUnit {}
 
 #[test]
 fn test_control_register() {
