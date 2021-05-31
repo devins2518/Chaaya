b491f4cfc276342d9c4905714eb27ff8406e4ef0
Devin Singh <drsingh2518@icloud.com>
2021-05-31 09:14:03 -0500

    update gyro and build files for libs

--- lib/smrti/build.zig
+++ lib/smrti/build.zig
@@ -1,15 +1,17 @@
 const std = @import("std");
+const pkgs = @import("deps.zig").pkgs;
 
 pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const lib = b.addStaticLibrary("smrti", "src/main.zig");
+    const lib = b.addStaticLibrary("smrti", "src/lib.zig");
     lib.setBuildMode(mode);
+    pkgs.addAllTo(lib);
     lib.install();
 
-    var main_tests = b.addTest("src/main.zig");
+    var main_tests = b.addTest("src/lib.zig");
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
